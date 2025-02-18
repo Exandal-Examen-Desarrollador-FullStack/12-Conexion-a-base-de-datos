e8f334dc0a8ae9a684d948dd53ef772d11214278
Ismael Hurtado <ismaelhv@outlook.com>
2025-02-18 15:27:36 -0500

    ajsute

--- index.js
+++ index.js
@@ -3,14 +3,16 @@ const mongoose = require('mongoose');
 
 async function main() {
   try {
-    await mongoose.connect('mongodb+srv://exandal:3x4nd4l321@cluster0.20lgw.mongodb.net/', {
-      useUnifiedTopology: true
-    });
+    /* 
+    Se sugiere que el usuario y la contraseña sean almacenadas en variables de entorno .env
+    Por efecto de ejemplo se usa esta base de datos de ejemplo
+    */
+    await mongoose.connect('mongodb+srv://exandal:3x4nd4l321@cluster0.20lgw.mongodb.net/');
     console.log('Conectado a MongoDB');
 
     const userSchema = new mongoose.Schema({
       nombre: String,
-      edad: Number
+      edad: Number,
     });
     const User = mongoose.model('User', userSchema);
 
